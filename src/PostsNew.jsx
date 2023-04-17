a8bbfe0109222f6b9fff7173163001cac4ab9342
“Edward <eminaya49@gmail.com>
2023-04-17 12:22:12 -0500

    modal complete for show of each individual post

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -2,13 +2,21 @@ export function PostsNew() {
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form action="">
-        Title
-        <input type="text" name="" id="" />
-      </form>
-      <form action="">
-        Body <input type="text" name="" id="" />
-      </form>
+      <div className="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">
+          Title
+        </span>
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-sm"
+        />
+      </div>
+      <div class="form-floating">
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+        <label for="floatingTextarea">Body</label>
+      </div>
       <form action="">
         Image <input type="text" name="" id="" />
       </form>
