977d3a4d027a83f67cc87f03d919d9dd3388672f
“Edward <eminaya49@gmail.com>
2023-04-18 14:02:45 -0500

    updated postShow to handle update post request

--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,9 +1,28 @@
+import axios from "axios";
+
 export function PostShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.patch("http://localhost:3000/posts/" + props.post.id + ".json", params).then((response) => {
+      console.log(response.data);
+      event.target.reset();
+    });
+  };
   return (
     <div>
-      <h3>Title: {props.post.title}</h3>
-      <p>Body: {props.post.body}</p>
-      <img src={props.post.image}></img>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Title: <input name="title" defaultValue={props.post.title} type="text" />
+        </div>
+        <div>
+          Body: <input name="body" defaultValue={props.post.body} type="text" />
+        </div>
+        <div>
+          Image: <input name="image" defaultValue={props.post.image} type="text" />
+        </div>
+        <button type="submit">Edit</button>
+      </form>
     </div>
   );
 }
